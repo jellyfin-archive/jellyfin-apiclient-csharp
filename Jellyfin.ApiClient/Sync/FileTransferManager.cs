58f19c34e409cefdd6c3e9a11fcc17203a05d48c
chaosinnovator <thomas.boland@outlook.com>
2019-12-10 12:45:45 -0800

    Missed a namespace change

--- Jellyfin.ApiClient/Sync/FileTransferManager.cs
+++ Jellyfin.ApiClient/Sync/FileTransferManager.cs
@@ -6,7 +6,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Emby.ApiClient.Sync
+namespace Jellyfin.ApiClient.Sync
 {
     class FileTransferManager : IFileTransferManager
     {
